3a6810e6737e66eff4c5daec2bf335643f2f8236
Atharva Mene <atharvamene@gmail.com>
2025-09-03 00:17:23 +0530

    updates 2

--- app.js
+++ app.js
@@ -20,9 +20,18 @@ function btnFlash(btn) {
   btn.classList.add("flash");
   setTimeout(function () {
     btn.classList.remove("flash");
-  }, 500);
+  }, 300);
 }
+
+function userFlash(btn) {
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
+  }, 100);
+}
+
 function levelUp() {
+  userSequence = [];
   level++;
   h2.innerText = `Level: ${level}`;
 
@@ -31,5 +40,41 @@ function levelUp() {
   let randomIndx = Math.floor(Math.random() * 4);
   let randomColor = btns[randomIndx];
   let randBtn = document.querySelector(`.${randomColor}`);
+  // console.log(randomIndx);
+  // console.log(randBtn);
+  // console.log(randomColor);
+  gameSequence.push(randomColor);
   btnFlash(randBtn);
+}
+
+function checkAns(idx) {
+  if (userSequence[idx] == gameSequence[idx]) {
+    if (userSequence.length == gameSequence.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerText = "Game Over! Press Any Key To Restart The Game";
+    reset();
+  }
+}
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+
+  userColor = btn.getAttribute("id");
+  userSequence.push(userColor);
+  checkAns(userSequence.length - 1);
+}
+
+let allBtn = document.querySelectorAll(".btn");
+for (btn of allBtn) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  let gameSequence = [];
+  let userSequence = [];
+  level = 0;
 }
